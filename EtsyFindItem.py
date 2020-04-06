ff1297b56e022ec7a476043df248dc82fe46fbe9
BZhong7 <Zhong.Brandon26@gmail.com>
2020-04-06 18:47:19 -0400

    Combined all functions into single function

--- EtsyFindItem.py
+++ EtsyFindItem.py
@@ -4,7 +4,9 @@ import os
 
 from ebaysdk.finding import Connection as finding
 
-def ebay_listing(event):
+def item_finder(event, context):
+
+#-----------------eBay Finding API------------------    
     search_terms = '('
     for x in event["multiValueQueryStringParameters"]["brands"]:
         search_terms += '"' + x + '"' + ','
@@ -20,46 +22,40 @@ def ebay_listing(event):
             #aspectFilter is CASE SENSITIVE. Wording must also be EXACT.
             #'aspectFilter': event["aspectFilters"]
         }
-    response = api.execute('findItemsByKeywords', api_request)
+    eBayResponse = api.execute('findItemsByKeywords', api_request)
 
     eBayDict = {}
-    for index, item in enumerate(response.reply.searchResult.item):
+    for index, item in enumerate(eBayResponse.reply.searchResult.item):
         eBayDict[index] = {}
         eBayDict[index]["title"] = item.title
-        #eBayDict[index]["price"] = item.sellingStatus.currentPrice.value
-        #eBayDict[index]["url"] = item.viewItemURL
-    
-    #print(json.dumps(eBayDict, indent=4))
-    return { json.dumps(eBayDict) }
- 
-#----------------------------------------------------------------
-def etsy_listing(event):
+        eBayDict[index]["price"] = item.sellingStatus.currentPrice.value
+        eBayDict[index]["url"] = item.viewItemURL
+
+
+#-----------------Etsy API--------------
     for brand in event["multiValueQueryStringParameters"]["brands"]:
         payload = {'api_key': os.environ['etsyapikey'],
             'fields':  'listing_id,title,price,url',
             'category': 'Clothing',
             'keywords': brand}
 
-        r = requests.get('https://openapi.etsy.com/v2/listings/active?', params=payload)
+        etsyResponse = requests.get('https://openapi.etsy.com/v2/listings/active?', params=payload)
 
-        data = {}
-        json_file = r.json()
-        for index, x in enumerate(json_file["results"]):
-            data[index] = {}
-            data[index]["title"] = x["title"]
-            data[index]["price"] = x["price"]
-            data[index]["url"] = x["url"]
+        etsyDict = {}
+        #json_file = r.json()
+        for index, x in enumerate(etsyResponse.json()["results"]):
+            etsyDict[index] = {}
+            etsyDict[index]["title"] = x["title"]
+            etsyDict[index]["price"] = x["price"]
+            etsyDict[index]["url"] = x["url"]
 
-    print(json.dumps(data, indent=4))
-    return { data.__dict__ }
 
-def item_finder(event, context):
-    ebay_str = ebay_listing(event)
-    #etsy_str = etsy_listing(event)
+#----------------Create and return response----------------
 
     responseObject = {}
     responseObject['headers'] = {}
     responseObject['headers']['Content-Type'] = 'application/json'
-    responseObject['body'] = ebay_str
+    responseObject['body']['ebay'] = json.dumps(eBayDict)
+    responseObject['body']['etsy'] = json.dumps(etsyDict)
 
     return responseObject
