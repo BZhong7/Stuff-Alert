f1516a3ec83c870447aba229f22930a246915fd1
BZhong7 <Zhong.Brandon26@gmail.com>
2020-04-17 18:06:01 -0400

    Added for loops to eBay API calls

--- EtsyFindItem.py
+++ EtsyFindItem.py
@@ -7,34 +7,37 @@ from ebaysdk.finding import Connection as finding
 def item_finder(event, context):
 
 #-----------------eBay Finding API------------------    
-    search_terms = '('
-    for x in event["multiValueQueryStringParameters"]["brands"]:
-        search_terms += '"' + x + '"' + ','
-    search_terms += ')'
+    eBayDict = {}
+    for brand in event["multiValueQueryStringParameters"]["brands"]:
+        eBayDict[brand] = {}
+        for tag in event["multiValueQueryStringParameters"]["tags"]:
+            search_terms = brand + " " + tag
 
-    Keywords = search_terms
-    api = finding(appid=os.environ["ebayapikey"], config_file=None)
-    api_request = { 'keywords': Keywords, 
-            'paginationInput': {
-                'entriesPerPage': 10,
-                'pageNumber': 1
+            Keywords = search_terms
+            api = finding(appid=os.environ["ebayapikey"], config_file=None, https=True)
+            api_request = { 'keywords': Keywords,
+                'paginationInput': {
+                    'entriesPerPage': 10,
+                    'pageNumber': 1
+                },
+                #aspectFilter is CASE SENSITIVE. Wording must also be EXACT.
+                'aspectFilter': {
+                    'aspectName': "Size", 'aspectValueName': '32'
+                }
             }
-            #aspectFilter is CASE SENSITIVE. Wording must also be EXACT.
-            #'aspectFilter': event["aspectFilters"]
-        }
 
-    try:
-        eBayResponse = api.execute('findItemsByKeywords', api_request)
+            try:
+                eBayResponse = api.execute('findItemsByKeywords', api_request)
 
-        eBayDict = {}
-        for index, item in enumerate(eBayResponse.reply.searchResult.item):
-            eBayDict[index] = {}
-            eBayDict[index]["title"] = item.title
-            eBayDict[index]["price"] = item.sellingStatus.currentPrice.value
-            eBayDict[index]["url"] = item.viewItemURL
-    except:
-        eBayDict = {}
-        eBayDict["error"] = "Unable to connect..."
+                eBayDict[brand][tag] = {}
+                for index, item in enumerate(eBayResponse.reply.searchResult.item):
+                    eBayDict[brand][tag][index] = {}
+                    eBayDict[brand][tag][index]["title"] = item.title
+                    eBayDict[brand][tag][index]["price"] = item.sellingStatus.currentPrice.value
+                    eBayDict[brand][tag][index]["url"] = item.viewItemURL
+            except:
+                eBayDict[brand][tag] = {}
+                eBayDict[brand][tag]["error"] = "Unexpected error..."
 
 
 #-----------------Etsy API--------------
@@ -46,7 +49,8 @@ def item_finder(event, context):
                 'fields':  'listing_id,title,price,url',
                 'category': 'Clothing',
                 'tags': tag,
-                'keywords': brand}
+                'keywords': brand,
+            }
 
             try:
                 etsyResponse = requests.get('https://openapi.etsy.com/v2/listings/active?',
